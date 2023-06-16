3773884c6ccbb26190a0949ee5e195be6395382a
Anshika1603 <anshika.srivastava@knoldus.com>
2023-06-16 12:45:42 +0530

    Web app files

---
+++ src/test/java/com/knoldus/appservice/AppserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.appservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
